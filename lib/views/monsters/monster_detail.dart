9b5fa913cbd3cf9dc53f7efc35bb6d3a405e7fca
lucpn <ngothienlo@gmail.com>
2020-12-06 14:46:13 +0700

    [ADD] - Initial Monster detail page

--- lib/views/monsters/monster_detail.dart
+++ lib/views/monsters/monster_detail.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:monster_hunter_world_wiki/models/models.dart';
 
 class MonsterDetailScreen extends StatefulWidget {
-  MonsterDetailScreen({Key key}) : super(key: key);
+  final Monster monster;
+  MonsterDetailScreen({Key key, this.monster}) : super(key: key);
 
   @override
   _MonsterDetailScreenState createState() => _MonsterDetailScreenState();
@@ -11,9 +13,27 @@ class _MonsterDetailScreenState extends State<MonsterDetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Container(
-        child: Text('Detail Page'),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            renderHeader(widget.monster.id, widget.monster.name),
+            renderBody([widget.monster.description]),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget renderHeader(int monsterID, String monsterName) {
+    return Column(
+      children: [],
+    );
+  }
+
+  Widget renderBody(List<dynamic> bodyDatas) {
+    return Container(
+      child: Text('Monster info'),
     );
   }
 }
